7dfd7180725f0604c6eb32f2ecdffd97ba8a5270
Didier Delhaisse <did35@yahoo.com>
2025-02-09 10:14:07 +0100

    Initial Commit

---
+++ TimeNow/TimeNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeNowApp.swift
+//  TimeNow
+//
+//  Created by Didier Delhaisse on 09/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct TimeNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
